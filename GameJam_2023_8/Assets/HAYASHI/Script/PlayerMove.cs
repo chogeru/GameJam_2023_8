428fffc87b466bab49e3e14196328966ca837748
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2023-08-10 16:08:46 +0900

    スクリプト追加

--- GameJam_2023_8/Assets/HAYASHI/Script/PlayerMove.cs
+++ GameJam_2023_8/Assets/HAYASHI/Script/PlayerMove.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // 加速度のレート
-    [SerializeField]
-    private float m_AccelerationRate = 10f;
-    // 最大速度
-    [SerializeField]
-    private float m_MaxSpeed = 25f; 
-    [SerializeField]
-    private float m_CurrentSpeed = 0f; // 現在の速度
-    [SerializeField]
-    private bool isAccelerating = false; // ボタンを押しているかどうかのフラグ
+    [SerializeField,Header("加速度のレート")]
+    private float m_AccelerationRate = 5f;
+    [SerializeField,Header("最大スピード")]
+    private float m_MaxSpeed = 25f;
+    [SerializeField,Header("ジャンプ力")]
+    private float m_JumpForce = 10f;
+    [SerializeField,Header("ボタン押したときの回転値")]
+    private float m_RotationSpeed = 90f;
+    [SerializeField,Header("現在の速度")]
+    private float m_CurrentSpeed = 0f;
+    // ボタンを押しているかどうかのフラグ
+    private bool isAccelerating = false;
+    // 地面に接地しているかのフラグ
+    private bool isGrounded = true;
 
     private void Update()
     {
+        // 地面に接地しているかの判定
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.10f);
+
         // キー入力による加速・減速制御
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
@@ -25,7 +32,7 @@ public class PlayerMove : MonoBehaviour
         else
         {
             isAccelerating = false;
-            m_CurrentSpeed = Mathf.Max(m_CurrentSpeed - m_AccelerationRate * Time.deltaTime, 5f); // ボタンを離したら減速
+            m_CurrentSpeed = Mathf.Max(m_CurrentSpeed - m_AccelerationRate * Time.deltaTime, 0f); // ボタンを離したら減速
         }
 
         // 加速度の計算
@@ -34,11 +41,27 @@ public class PlayerMove : MonoBehaviour
             m_CurrentSpeed = Mathf.Min(m_CurrentSpeed + m_AccelerationRate * Time.deltaTime, m_MaxSpeed); // 最大速度を超えないように制限
         }
 
+        // ジャンプ処理
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
+        }
+
         // キー入力による移動処理
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * m_CurrentSpeed * Time.deltaTime;
         transform.Translate(movement);
+
+        // 回転処理
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -m_RotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, m_RotationSpeed * Time.deltaTime);
+        }
     }
 }

171b16a096db6eaddec2b1641a49a90ee8d71752
ABURAMUSI <108376848+chogeru@users.noreply.github.com>
2023-08-31 22:11:38 +0900

    修正

---
+++ GameJam_2023_8/Assets/HAYASHI/Script/RankCheckerHaya.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RankCheckerHaya : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] CarList;
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private GameObject[] CheckPoint;
+
+    [SerializeField]
+    private float[] CarRankCalc;
+
+    [SerializeField]
+    private int CheckPointNo = 0;
+
+    [SerializeField]
+    private float m_CheckTime=3;
+    [SerializeField]
+    private float m_Time;
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Time += Time.deltaTime;
+        if (m_Time > m_CheckTime)
+        {
+            CarList = GameObject.FindGameObjectsWithTag("Car");
+            CarRankCalc = new float[CarList.Length];
+        }
+        //チェックポイント確認
+        for (int i = 0; i < CheckPoint.Length; i++)
+        {
+            if (CheckPoint[i].activeSelf)
+            {
+                CheckPointNo = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < CarList.Length; i++)
+        {
+            Vector3 point = CheckPoint[CheckPointNo].transform.position;
+            //CarRankCalc[i] = (CarList[i].transform.position.z -  point.z);
+            CarRankCalc[i] = Vector3.Distance(CarList[i].transform.position, point);
+        }
+
+        //順位並び替え
+        for(int i=0;i< CarRankCalc.Length;i++)
+        {
+            for(int j = i+1; j < CarRankCalc.Length;j++)
+            {
+                if (CarRankCalc[i] > CarRankCalc[j])
+                {
+                    float tmp = CarRankCalc[i];
+                    CarRankCalc[i] = CarRankCalc[j];
+                    CarRankCalc[j] = tmp;
+
+                    GameObject obj = CarList[i];
+                    CarList[i] = CarList[j];
+                    CarList[j] = obj;
+                }
+            }
+        }
+    }
+
+    public string GetRankText()
+    {
+        string res = "";
+
+        for (int i = 0; i < CarList.Length; i++)
+        {
+            if(res != "")
+            {
+                res += " / ";
+            }
+            res += CarList[i].name.ToString();
+        }
+
+        return res;
+    }
+
+    public void ClearAllCheckPoint()
+    {
+        for(int i=0;i<CheckPoint.Length;i++)
+        {
+            CheckPoint[i].SetActive(true);
+        }
+    }
+}
